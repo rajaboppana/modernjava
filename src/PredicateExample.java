066c21e212d383c81fc9bca5fe9a9d1d0d6a8ace
rboppana <Ramani@3233>
2023-06-27 11:22:03 +0200

    Predicate commit

--- src/PredicateExample.java
+++ src/PredicateExample.java
@@ -1,2 +1,16 @@
+import java.util.function.Predicate;
+
 public class PredicateExample {
+    public static void main(String[] args) {
+
+        Predicate<Integer> p1 = (i) -> i>10;
+        System.out.println(p1.test(100));
+
+        Predicate<Integer> p2 = (i) -> i%2==0;
+        System.out.println(p1.and(p2).test(20));
+
+        System.out.println(p1.or(p2).test(4));
+
+        System.out.println(p1.or(p2.negate()).test(33));
+    }
 }
